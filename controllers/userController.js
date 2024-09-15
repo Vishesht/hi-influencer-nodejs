1210ff10c43a70713973fbc7129cf8165cda1430
Vishesht Gupta <vgupta@ccrn.com>
2024-09-15 17:35:17 +0530

    use details api done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const User = require("../models/User");
+const { v4: uuidv4 } = require("uuid");
 
 // Get a user by ID
 exports.getUser = async (req, res) => {
@@ -18,13 +19,41 @@ exports.getUser = async (req, res) => {
 };
 
 // Create a new user
-exports.createUser = async (req, res) => {
+exports.updateOrCreateUser = async (req, res) => {
   try {
-    const newUser = new User(req.body); // Use req.body directly if it matches the schema
-    await newUser.save();
-    res.status(201).json(newUser);
+    const { _id, email, ...optionalFields } = req.body;
+
+    if (!_id) {
+      return res
+        .status(400)
+        .json({ message: "User ID (_id) is required for update." });
+    }
+
+    // Check if the user with the provided _id exists
+    const existingUser = await User.findById(_id);
+    if (existingUser) {
+      // If user with _id exists, update the user
+      Object.assign(existingUser, { email, ...optionalFields });
+      await existingUser.save();
+      return res.status(200).json(existingUser);
+    } else {
+      // If user with _id does not exist, check if email is taken
+      const emailTaken = await User.findOne({ email });
+      if (emailTaken) {
+        // If email exists but _id does not match, return an error
+        return res
+          .status(400)
+          .json({
+            message: "Email already exists but does not match the provided ID",
+          });
+      }
+      // If email is not taken, create a new user
+      const newUser = new User({ email, ...optionalFields });
+      await newUser.save();
+      return res.status(201).json(newUser);
+    }
   } catch (error) {
-    console.error("Error creating user:", error);
+    console.error("Error updating or creating user:", error);
     res.status(400).json({ message: error.message });
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.post("/users", userController.createUser);
+router.put("/users", userController.updateOrCreateUser);
 router.get("/users/:id", userController.getUser);
 
 module.exports = router;
