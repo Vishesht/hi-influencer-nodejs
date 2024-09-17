293e7b103daed07e9e95ca89e6f856f8cb65968e
Vishesht Gupta <vgupta@ccrn.com>
2024-09-17 20:06:27 +0530

    vercel changes

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ mongoose
 
 // Routes
 const userRoutes = require("./routes/userRoutes");
+app.get("/", (req, res) => res.send("Server working."));
+
 app.use("/api", userRoutes);
 
 // Error Handling Middleware
@@ -42,3 +44,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
