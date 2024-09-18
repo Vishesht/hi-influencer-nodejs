ad3ea7f5123b7d4aa68c0ff5b29b269889dcc8df
Vishesht Gupta <vgupta@ccrn.com>
2024-09-18 15:40:37 +0530

    Add Ads APis

---
+++ models/Ads.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const adsSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  isInfluencer: { type: Boolean, required: true },
+  photoURL: { type: String },
+  adsImages: [{ type: String }],
+  state: { type: String },
+  title: { type: String, required: true },
+  desc: { type: String, required: true },
+  category: { type: String },
+  budget: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Ads = mongoose.model("Ads", adsSchema);
+
+// Ensure the index is dropped if it exists
+Ads.on("index", (error) => {
+  if (error) {
+    console.error("Index error:", error);
+  }
+});
+
+// Use syncIndexes to update the indexes in the database
+Ads.syncIndexes();
+
+module.exports = Ads;
