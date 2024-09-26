a5a400f6befdc8024fd8de0a12e54b92f40994bb
Vishesht Gupta <vgupta@ccrn.com>
2024-09-26 23:22:58 +0530

    chat integration done

--- routes/notificationService.js
+++ routes/notificationService.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { sendNotification } = require("../controllers/notificationController");
+const { createChat, getUserChats } = require("../controllers/chatController");
 
 router.post("/send-notification", sendNotification);
+router.post("/create-chat", createChat);
+router.get("/chat/:userId", getUserChats);
 
 module.exports = router;
