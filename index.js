5a0dc7d3a0a0dae775160f89b2d2ab6f4f1af7bc
Vishesht Gupta <vgupta@ccrn.com>
2024-10-04 17:41:22 +0530

    adder hiinfluencer url

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "https://hi-influencer-web-qd5q.vercel.app",
+      "https://hiinfluencer.in"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
