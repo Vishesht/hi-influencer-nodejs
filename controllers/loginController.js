3db4134e6fa68e50417e3df44ef686d40131e967
Vishesht Gupta <vgupta@ccrn.com>
2024-10-05 15:19:50 +0530

    login already user fix

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -241,6 +241,12 @@ exports.changePassword = async (req, res) => {
 exports.sendNewUserOtp = async (req, res) => {
   const { email } = req.body;
   try {
+    const existingUser = await LoginModel.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User already exists. Please log in." });
+    }
     const otp = generateOtp();
     await Otp.create({ email, otp, createdAt: Date.now() });
     await transporter.sendMail({
